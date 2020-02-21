bc1881b990b520fc797c711386de7e50b7285d66
frankfang <frankfang1990@gmail.com>
2020-02-21 15:57:06 +0800

    deploy

---
+++ precache-manifest.6a0c22afcf974a3609d3b8ee81c22c84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b962b67b13f6aa7023a5",
+    "url": "/morney-3-website/css/app.214c3c06.css"
+  },
+  {
+    "revision": "b30addc6f5f56dd129793e62f23c6171",
+    "url": "/morney-3-website/index.html"
+  },
+  {
+    "revision": "b962b67b13f6aa7023a5",
+    "url": "/morney-3-website/js/app.4ead2f5e.js"
+  },
+  {
+    "revision": "25aa90c49f430c163e21",
+    "url": "/morney-3-website/js/chunk-vendors.3dc4100e.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/morney-3-website/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/morney-3-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-3-website/robots.txt"
+  }
+]);
